15960ebbfd54f135d65b66f091d4dbbba419ae69
dvillarrealm96 <66176367+dvillarrealm96@users.noreply.github.com>
2020-05-31 15:14:15 -0500

    Unidad 4

--- Main/src/app/app.routes.ts
+++ Main/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { Unidad1Component } from "./components/unidad1/unidad1.component";
 import { Unidad2Component } from "./components/unidad2/unidad2.component";
 import { Unidad3Component } from './components/unidad3/unidad3.component';
+import { Unidad4Component } from './components/unidad4/unidad4.component';
 
 
 const APP_ROUTES: Routes = [
@@ -10,7 +11,10 @@ const APP_ROUTES: Routes = [
     { path: 'inicio', component: InicioComponent },
     { path: 'Unidad1', component: Unidad1Component },
     { path: 'Unidad2', component: Unidad2Component },
-    {path: 'unidad3', component: Unidad3Component}
+    { path: 'Unidad3', component: Unidad3Component },
+    { path: 'Unidad4', component: Unidad4Component }
+
+
 
 ];
 
