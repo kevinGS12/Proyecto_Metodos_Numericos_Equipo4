4540edaa87cd7bc394d8d27291e1e67a3d7a9c67
kevinGS12 <kevin_12208@hotmail.com>
2020-05-31 14:05:54 -0500

    unidad 3
    preguntas sobre unidad 3

--- Main/src/app/app.routes.ts
+++ Main/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes, RouterModule } from "@angular/router";
 import { InicioComponent } from './components/inicio/inicio.component';
 import { Unidad1Component } from "./components/unidad1/unidad1.component";
+import { Unidad3Component } from './components/unidad3/unidad3.component';
 
 const APP_ROUTES: Routes = [
     { path: '', component: InicioComponent },
     { path: 'inicio', component: InicioComponent },
-    { path: 'Unidad1', component: Unidad1Component }
+    { path: 'Unidad1', component: Unidad1Component },
+
+    {path: 'unidad3', component: Unidad3Component}
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
