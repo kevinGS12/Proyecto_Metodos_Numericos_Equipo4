dd7f84891c911a622d19b43f61cd32ae41f7d541
dvillarrealm96 <66176367+dvillarrealm96@users.noreply.github.com>
2020-05-31 14:11:29 -0500

    Unidad 2
    Creación unidad 2

--- Main/src/app/app.module.ts
+++ Main/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { Unidad1Component } from './components/unidad1/unidad1.component';
+import { Unidad2Component } from './components/unidad2/unidad2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     InicioComponent,
-    Unidad1Component
+    Unidad1Component,
+    Unidad2Component
   ],
   imports: [
     BrowserModule,
--- Main/src/app/app.routes.ts
+++ Main/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from "@angular/router";
 import { InicioComponent } from './components/inicio/inicio.component';
 import { Unidad1Component } from "./components/unidad1/unidad1.component";
+import { Unidad2Component } from "./components/unidad2/unidad2.component";
 
 const APP_ROUTES: Routes = [
     { path: '', component: InicioComponent },
     { path: 'inicio', component: InicioComponent },
-    { path: 'Unidad1', component: Unidad1Component }
+    { path: 'Unidad1', component: Unidad1Component },
+    { path: 'Unidad2', component: Unidad2Component }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
